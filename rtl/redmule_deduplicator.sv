be9c91682cc588976db309c68f635b3eb860618d
Maurus Item <itemm@student.ethz.ch>
2024-07-29 12:48:30 +0200

    Added more stuff to deduplicator

--- rtl/redmule_deduplicator.sv
+++ rtl/redmule_deduplicator.sv
@@ -1,8 +1,8 @@
 /*
  * Copyright (C) 2024-2024 ETH Zurich and University of Bologna
  *
- * Licensed under the Solderpad Hardware License, Version 0.51 
- * (the "License"); you may not use this file except in compliance 
+ * Licensed under the Solderpad Hardware License, Version 0.51
+ * (the "License"); you may not use this file except in compliance
  * with the License. You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
@@ -15,14 +15,14 @@
  * SPDX-License-Identifier: SHL-0.51
  *
  * Authors:  Maurus Item     <itemm@student.ethz.ch>
- * 
+ *
  * RedMulE TCDM Deduplication Cache
  */
 
 `include "hci_helpers.svh"
 `include "common_cells/registers.svh"
 
-module redmule_deduplicator 
+module redmule_deduplicator
   import redmule_pkg::*;
   import hci_package::*;
   import hwpe_stream_package::*;
@@ -35,8 +35,12 @@ module redmule_deduplicator
   hci_core_intf.initiator tcdm_initiator
 );
 
-  localparam int unsigned AW  = `HCI_SIZE_GET_AW(tcdm);
+  localparam int unsigned AW = `HCI_SIZE_GET_AW(tcdm);
   localparam int unsigned DW = `HCI_SIZE_GET_DW(tcdm);
+  localparam int unsigned EW = `HCI_SIZE_GET_EW(tcdm);
+  localparam int unsigned UW = `HCI_SIZE_GET_UW(tcdm);
+  localparam int unsigned IW = `HCI_SIZE_GET_IW(tcdm);
+  localparam int unsigned EHW = `HCI_SIZE_GET_EHW(tcdm);
 
   //////////////////////////////////////////////////////////////////////////////////
   // Forward Deduplication
@@ -56,7 +60,7 @@ module redmule_deduplicator
   always_comb begin
     if (drop_request) begin
       tcdm_initiator.req  = 1'b0;
-      tcdm_target.gnt     = 1'b1;   
+      tcdm_target.gnt     = 1'b1;
     end else begin
       tcdm_initiator.req  = tcdm_target.req;
       tcdm_target.gnt     = tcdm_initiator.gnt;
@@ -95,23 +99,48 @@ module redmule_deduplicator
   logic replicate_read;
   assign replicate_read = pickup_read && !empty;
 
-  assign response_in = tcdm_initiator.r_valid && !empty && tcdm_initiator.r_ready;
+  assign response_in = tcdm_initiator.r_valid && !empty; // && tcdm_initiator.r_ready; Ready not used in upstream!
   assign response_out = response_in || replicate_read;
 
   // Store data whenever there is a request recieved
-  logic [DW-1:0] r_data_q;
-  `FFL(r_data_q, tcdm_initiator.r_data, response_in, '0);
+  logic  [DW-1:0] r_data_q;
+  logic  [UW-1:0] r_user_q;
+  logic  [IW-1:0] r_id_q;
+  logic           r_opc_q;
+  logic  [EW-1:0] r_ecc_q;
+  logic [EHW-1:0] r_evalid_q;
+
+  `FFL(  r_data_q, tcdm_initiator.r_data,   response_in, '0);
+  `FFL(  r_user_q, tcdm_initiator.r_user,   response_in, '0);
+  `FFL(    r_id_q, tcdm_initiator.r_id,     response_in, '0);
+  `FFL(   r_opc_q, tcdm_initiator.r_opc,    response_in, '0);
+  `FFL(   r_ecc_q, tcdm_initiator.r_ecc,    response_in, '0);
+  `FFL(r_evalid_q, tcdm_initiator.r_evalid, response_in, '0);
 
   // Data / Handshake Injection
   always_comb begin
     if (replicate_read) begin
-      tcdm_initiator.r_ready = 1'b0;
-      tcdm_target.r_valid = 1'b1;
-      tcdm_target.r_data = r_data_q;
+      tcdm_initiator.r_ready  = 1'b0;
+      tcdm_initiator.r_eready = '0;
+
+      tcdm_target.r_valid     = 1'b1;
+      tcdm_target.r_evalid    = r_evalid_q;
+      tcdm_target.r_data      = r_data_q;
+      tcdm_target.r_ecc       = r_ecc_q;
+      tcdm_target.r_user      = r_user_q;
+      tcdm_target.r_id        = r_id_q;
+      tcdm_target.r_opc       = r_opc_q;
     end else begin
-      tcdm_initiator.r_ready = tcdm_target.r_ready;
-      tcdm_target.r_valid = tcdm_initiator.r_valid;
-      tcdm_target.r_data = tcdm_initiator.r_data;
+      tcdm_initiator.r_ready  = tcdm_target.r_ready;
+      tcdm_initiator.r_eready = tcdm_target.r_eready;
+
+      tcdm_target.r_valid     = tcdm_initiator.r_valid;
+      tcdm_target.r_evalid    = tcdm_initiator.r_evalid;
+      tcdm_target.r_data      = tcdm_initiator.r_data;
+      tcdm_target.r_ecc       = tcdm_initiator.r_ecc;
+      tcdm_target.r_user      = tcdm_initiator.r_user;
+      tcdm_target.r_id        = tcdm_initiator.r_id;
+      tcdm_target.r_opc       = tcdm_initiator.r_opc;
     end
   end
 
@@ -122,16 +151,12 @@ module redmule_deduplicator
   assign tcdm_initiator.be      = tcdm_target.be;
   assign tcdm_initiator.user    = tcdm_target.user;
   assign tcdm_initiator.id      = tcdm_target.id;
-  assign tcdm_target.r_user     = tcdm_initiator.r_user;
-  assign tcdm_target.r_id       = tcdm_initiator.r_id;
-  assign tcdm_target.r_opc      = tcdm_initiator.r_opc;
+
 
   // ECC signals
   assign tcdm_initiator.ereq     = tcdm_target.ereq;
   assign tcdm_target.egnt        = tcdm_initiator.egnt;
-  assign tcdm_target.r_evalid    = tcdm_initiator.r_evalid;
-  assign tcdm_initiator.r_eready = tcdm_target.r_eready;
+
   assign tcdm_initiator.ecc      = tcdm_target.ecc;
-  assign tcdm_target.r_ecc       = tcdm_initiator.r_ecc;
 
 endmodule // redmule_deduplicator
