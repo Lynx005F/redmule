6a3637443f50c4ebaef1cc6c78d0616142aa9eb1
Maurus Item <itemm@student.ethz.ch>
2025-03-01 14:46:36 +0100

    Modified setup.sh to use the same gcc as Philip

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,8 +3,8 @@ echo "Exporting bender path to $BENDER_DIR"
 export PATH=$BENDER_DIR:$PATH
 unset BENDER_DIR
 echo "Exporting SDK and GCC Toolchain paths"
-export PATH=/usr/pack/pulpsdk-1.0-kgf/artifactory/pulp-sdk-release/pkg/pulp_riscv_gcc/1.0.16/bin:$PATH
-export PULP_RISCV_GCC_TOOLCHAIN=/usr/pack/pulpsdk-1.0-kgf/artifactory/pulp-sdk-release/pkg/pulp_riscv_gcc/1.0.16
+export PATH=/usr/pack/riscv-1.0-kgf/pulp-gcc-2.6.0/bin:$PATH
+export PULP_RISCV_GCC_TOOLCHAIN=/usr/pack/riscv-1.0-kgf/pulp-gcc-2.6.0
 export PULP_CC=riscv32-unknown-elf-gcc
 export PULP_LD=riscv32-unknown-elf-gcc
 export PATH=/usr/pack/gcc-5.2.0-af/x86_64-rhe6-linux/bin:$PATH
